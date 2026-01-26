66e58d3661f3d4bc797d8e010d2214ce60e00f3e
Daria <daria.s.bulatova@sandboxer.ru>
2026-01-26 15:20:50 +0300

    fix icon in toast

--- src/shared/ui/Toast/Toast.tsx
+++ src/shared/ui/Toast/Toast.tsx
@@ -5,22 +5,28 @@
 
 import React from 'react'
 import { View, Text } from 'react-native'
-import Octicons from '@expo/vector-icons/Octicons';
+import Octicons from '@expo/vector-icons/Octicons'
 import type { ToastProps } from './types'
 import { Icon } from '@/shared/ui'
 
-export const Toast: React.FC<ToastProps> = ({ message, description, variant = 'success', ...restProp  }) => {
-
-	//const iconColor = variant === 'success' ? '#81BD12' : variant === 'error' ? '#CA3114' : '#1A3FBA'
-
-	console.log('restProp')
-	console.log(restProp)
+export const Toast: React.FC<ToastProps> = ({
+	message,
+	description,
+	variant = 'success',
+	iconName,
+}) => {
+	const iconColor =
+		variant === 'success' ? '#81BD12' : variant === 'error' ? '#CA3114' : '#1A3FBA'
 
 	return (
 		<View className="flex-row items-center gap-5 rounded-2xl bg-fill-800 p-4 shadow-lg w-full ">
 
-			<View className="p-4  rounded-2xl bg-fill-700" >
-				{/*{ iconName ? <Icon name={iconName} size={14} color={iconColor} /> : <Octicons name="dot-fill" size={14} color={iconColor} /> }*/}
+			<View className="p-4  rounded-2xl bg-fill-700">
+				{iconName ? (
+					<Icon name={iconName} size={14} color={iconColor} />
+				) : (
+					<Octicons name="dot-fill" size={14} color={iconColor} />
+				)}
 			</View>
 			<View className="flex-column gap-5" >
 				<Text className="flex-1 text-t2-bold text-light-text-100">{message}</Text>
--- src/shared/ui/Toast/config.tsx
+++ src/shared/ui/Toast/config.tsx
@@ -3,10 +3,20 @@ import { Toast } from './Toast'
 import { type ToastConfig } from 'react-native-toast-message'
 
 export const toastConfig: ToastConfig = {
-	success: ({ text1, text2, iconName }: any) =>(
-		<Toast variant="success" message={text1} description={text2} iconName={iconName} />
+	success: ({ text1, text2, props }: any) => (
+		<Toast
+			variant="success"
+			message={text1}
+			description={text2}
+			iconName={props?.iconName}
+		/>
 	),
-	error: ({ text1, text2, iconName }: any) => (
-		<Toast variant="error" message={text1} description={text2} iconName={iconName} />
+	error: ({ text1, text2, props }: any) => (
+		<Toast
+			variant="error"
+			message={text1}
+			description={text2}
+			iconName={props?.iconName}
+		/>
 	),
 }
